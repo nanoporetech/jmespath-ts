9f3dbabbe64da874e53e849a42b1433bd7ecbd86
Glen Van Ginkel <glen.vanginkel@nanoporetech.com>
2020-05-04 09:37:50 +0100

    Updates to packages and package dependencies

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
